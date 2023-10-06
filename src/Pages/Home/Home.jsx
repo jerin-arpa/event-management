f32da46caeeb1f57e8eda34ccfc31d8a41eb4039
jerin-arpa <ismatjerrin3@gmail.com>
2023-10-06 22:43:04 +0600

    project initial setup

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Banner from "../../Components/Banner/Banner";
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <h2>Home</h2>
+        </div>
+    );
+};
+
+export default Home;
